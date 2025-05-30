86a00635ae1c4de08d3c258fa3cbef4f6f6063b1
JSandaruwan98 <sandru98093@gmail.com>
2025-05-30 16:48:13 +0530

    add: routing change

--- index.js
+++ index.js
@@ -6,15 +6,15 @@ app.get('/', (req, res) => {
   res.send('Hello GET!')
 })
 
-app.post('/', (req, res) => {
+app.post('/post', (req, res) => {
   res.send('Hello POST!')
 })
 
-app.put('/', (req, res) => {
+app.put('/put', (req, res) => {
   res.send('Hello PUT!')
 })
 
-app.delete('/', (req, res) => {
+app.delete('/delete', (req, res) => {
   res.send('Hello DELETE!')
 }) 
 
